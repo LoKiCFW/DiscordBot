1d78084961e183611ebf514ea81ff0143910f487
LoKiCFW <46649161+LoKiCFW@users.noreply.github.com>
2019-11-11 00:48:36 +0100

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const Discord   = require("discord.js");
-const client    = require("Discord.Client");
-
-client.on('ready', () => {
-  console.log('ready');
-})
-
-client.login(process.env.BOT_TOKEN);
